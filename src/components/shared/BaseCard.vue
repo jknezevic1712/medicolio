161da0147b66d282ae8cc7538f2a939bccbe93ea
Jakov Knezevic <knezevic.jakov@gmail.com>
2024-06-09 18:41:26 +0200

    Changes:
    BaseCard now has optional slot for actions.
    Added card action to home view for adding new patients.

--- src/components/shared/BaseCard.vue
+++ src/components/shared/BaseCard.vue
@@ -6,24 +6,36 @@ const props = defineProps<{
 
 <template>
   <div class="card">
-    <h1 v-if="props.title">{{ props.title }}</h1>
-    <slot></slot>
+    <div class="card-header">
+      <h1 v-if="props.title">{{ props.title }}</h1>
+      <slot name="actions" />
+    </div>
+    <slot />
   </div>
 </template>
 
 <style lang="sass" scoped>
 .card
+  width: 100%
+  max-width: 80rem
   border-radius: 12px
   box-shadow: 0 2px 8px $base-color-4-dimmed
   padding: 1rem
   margin: 2rem auto
-  max-width: 80rem
+
+.card-header
+  margin: 0 0 3rem 0
+  width: 100%
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-direction: column
+  @media screen and (min-width: $breakpoint-md)
+    justify-content: space-between
+    flex-direction: row
 
   h1
-    margin: 0 0 3rem 0
-    width: 100%
-    display: flex
-    justify-content: center
+    margin: 0 0 1rem 0
     @media screen and (min-width: $breakpoint-md)
-      justify-content: flex-start
+      margin: 0
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,9 @@ import PatientsList from '@/components/home/PatientsList.vue'
 <template>
   <section>
     <base-card title="Your patients">
+      <template #actions>
+        <base-button class="card-action">New Patient</base-button>
+      </template>
       <div class="patients-list-container">
         <patients-list />
       </div>
@@ -18,4 +21,9 @@ import PatientsList from '@/components/home/PatientsList.vue'
   display: flex
   justify-content: center
   align-items: center
+
+.card-action
+  width: 100%
+  @media screen and (min-width: $breakpoint-md)
+      width: fit-content
 </style>
